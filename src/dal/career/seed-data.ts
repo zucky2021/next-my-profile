1a53e9d6f2764a5ffd68cbe6877911a05d4e8e23
Zucky <usupfv@gmail.com>
2025-08-29 09:39:38 +0900

    7 Add career data

--- src/dal/career/seed-data.ts
+++ src/dal/career/seed-data.ts
@@ -7,4 +7,21 @@ export const careerSeedData = [
     endDate: new Date("2021-07-31"),
     description: "自動車部品、エナジーソリューション関連機器の製造販売",
   },
+  {
+    company: "株式会社コアテック",
+    url: "https://core-tech.jp/",
+    employmentType: "FULL_TIME",
+    startDate: new Date("2021-12-01"),
+    endDate: new Date("2024-12-31"),
+    description: "Webインテグレーション",
+  },
+  {
+    company: "jinjer株式会社",
+    url: "https://core-tech.jp/",
+    employmentType: "FULL_TIME",
+    startDate: new Date("2021-12-01"),
+    endDate: null,
+    description:
+      "人事労務・勤怠管理・給与計算・ワークフロー・経費精算など、人事労務の効率化を支援するクラウドサービスを開発・提供",
+  },
 ];
