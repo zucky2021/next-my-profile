aad92bc474113ac4c0c2c98087e81e10d16bffad
Zucky <usupfv@gmail.com>
2025-12-22 23:27:08 +0900

    Set prisma v7

--- src/shared/lib/prisma.ts
+++ src/shared/lib/prisma.ts
@@ -1,12 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
 const prisma =
   global.prisma ||
   new PrismaClient({
+    adapter,
     // ログ出力など必要ならオプション追加 (任意)
     log: ["query", "info", "warn", "error"],
   });
